fe94f23538cb5d3bbb3c0a25a5c2a0d002b85c31
Taos Transue <spacechips@rocketmail.com>
2023-11-25 03:22:51 +0000

    mean and mad scaling

--- examples/qm9/qm9.py
+++ examples/qm9/qm9.py
@@ -46,19 +46,24 @@ class SelectProperty(T.BaseTransform):
         return data
 
 
-# Update each sample prior to loading.
-def qm9_pre_transform(data):
-    # Set descriptor as element type.
-    data.x = data.z.float().view(-1, 1)
-    # Only predict free energy (index 10 of 19 properties) for this run.
-    data.y = data.y[:, 10] / len(data.x)
-    graph_features_dim = [1]
-    node_feature_dim = [1]
-    return data
+class ScaleMADMean(T.BaseTransform):
+    def __init__(self, mad, mean):
+        self.mad, self.mean = mad, mean
+
+    def __call__(self, data):
+        data.y = (data.y - self.mean) / self.mad
+
+        return data
 
 
-def qm9_pre_filter(data):
-    return data.idx < num_samples
+def forward_scale_shift_wrapper(model, scale, shift):
+    forward = model.forward
+    def forward_wrapped(data):
+        out = forward(data)
+        if model.training:
+            return out
+        return [scale * out[0] + shift]
+    return forward_wrapped
 
 
 # Set this path for output.
@@ -77,7 +82,7 @@ verbosity = config["Verbosity"]["level"]
 var_config = config["NeuralNetwork"]["Variables_of_interest"]
 
 # Always initialize for multi-rank training.
-world_size, world_rank = hydragnn.utils.setup_ddp()
+world_size, world_rank = hydragnn.utils.setup_ddp(master_port='9001')
 
 log_name = "qm9_test"
 # Enable print to log file.
@@ -87,32 +92,37 @@ hydragnn.utils.setup_log(log_name)
 # Filter function above used to run quick example.
 # NOTE: data is moved to the device in the pre-transform.
 # NOTE: transforms/filters will NOT be re-run unless the qm9/processed/ directory is removed.
-dataset = torch_geometric.datasets.QM9(
-    root="dataset/qm9", # pre_transform=qm9_pre_transform, pre_filter=qm9_pre_filter
-)
+dataset = torch_geometric.datasets.QM9(root="dataset/qm9")
 # EGNN splits
 dataset_rng = np.random.RandomState(seed=0)
 permutation = dataset_rng.permutation(len(dataset))
 dataset = dataset[permutation]
-splits = np.split(permutation, [100_000, 100_000 + 17_748])
-max_atomic_number = dataset[splits[0]].data.x[:, 5].max().item()
-dataset.transform = T.Compose([SelectProperty(1),
+train, val, test = np.split(permutation, [100_000, 100_000 + 17_748])
+max_atomic_number = dataset[train].data.x[:, 5].max().item()
+property_idx = 1
+mean = dataset[train].data.y[:, property_idx].mean()
+mad = (dataset[train].data.y[:, property_idx] - mean).abs().mean()
+dataset.transform = T.Compose([SelectProperty(property_idx),
                                CormorantFeatures(max_atomic_number, 2)])
+train, val, test = dataset[train], dataset[val], dataset[test]
+train.transform = T.Compose([train.transform, ScaleMADMean(mad, mean)])
 (train_loader, val_loader, test_loader,) = hydragnn.preprocess.create_dataloaders(
-    *map(dataset.__getitem__, splits), config["NeuralNetwork"]["Training"]["batch_size"]
+    train, val, test, config["NeuralNetwork"]["Training"]["batch_size"]
 )
 
 config = hydragnn.utils.update_config(config, train_loader, val_loader, test_loader)
-
 model = hydragnn.models.create_model_config(
     config=config["NeuralNetwork"],
     verbosity=verbosity,
 )
-breakpoint()
+model.forward = forward_scale_shift_wrapper(model, mad, mean)
 model = hydragnn.utils.get_distributed_model(model, verbosity)
 
-learning_rate = config["NeuralNetwork"]["Training"]["Optimizer"]["learning_rate"]
-optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+optimizer = torch.optim.Adam(
+    model.parameters(),
+    lr=config["NeuralNetwork"]["Training"]["Optimizer"]["learning_rate"],
+    weight_decay=1e-16
+)
 scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
     optimizer, config["NeuralNetwork"]["Training"]["num_epoch"]
 )
